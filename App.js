80df7029579c5f5b1241772a149df229f0dc21b4
atk-sz <65167734+atk-sz@users.noreply.github.com>
2021-06-14 21:01:36 +0530

    homepage panner done

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Button, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import FlashMessage from 'react-native-flash-message';
 import HomeStack from './src/stacks/HomeStack';
 
 function NotificationsScreen({ navigation }) {
@@ -16,12 +17,16 @@ const Drawer = createDrawerNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeStack} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <View style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Drawer.Navigator initialRouteName="Home">
+          <Drawer.Screen name="Home" component={HomeStack} />
+          <Drawer.Screen name="Login" component={NotificationsScreen} />
+          <Drawer.Screen name="SignUp" component={NotificationsScreen} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+      <FlashMessage style={{ marginTop: 25 }} position="top" />
+    </View>
   );
 }
 
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,83 @@
 import * as React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, StyleSheet, ImageBackground, Text, View, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import homeBannerimage from '../images/slide-1.jpg'
 
 const Home = () => {
     const navigation = useNavigation()
-    return (<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>CSKAA | VIDEOCONFERENCING</Text>
-        <Button onPress={() => navigation.openDrawer()} title="Open Nav" />
-    </View>)
+    return (
+        <View style={styles.container}>
+            <ImageBackground source={homeBannerimage} style={styles.homeBannerImage}>
+                <View style={styles.titleContainer}><Text style={styles.homeBannerTitle}>CSKAA | VIDEOCONFERENCING</Text></View>
+                <View style={styles.captionContainer}><Text style={styles.caption}>WE ARE ALMOST <Text style={styles.textYellowColor}> READY TO LAUNCH</Text> OUR <Text style={styles.textYellowColor}>NEW VIDEOCONFERENCING PLATFORM!</Text></Text></View>
+                <View style={styles.btnsContainer}>
+                    <TouchableOpacity style={styles.signUpBtn}>
+                        <Text style={styles.signUp}>Sign Up</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.letsBeginBtn}>
+                        <Text style={styles.letsBegin}>Let's begin</Text>
+                    </TouchableOpacity>
+                </View>
+            </ImageBackground>
+        </View>
+    )
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        display: 'flex',
+        justifyContent: 'center',
+    },
+    homeBannerImage: {
+        flex: 1,
+        display: 'flex',
+        resizeMode: 'cover',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    homeBannerTitle: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 20
+    },
+    captionContainer: {
+        paddingVertical: 10,
+    },
+    caption: {
+        fontSize: 8,
+        fontWeight: 'bold',
+        color: 'white'
+    },
+    btnsContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    signUpBtn: {
+        margin:10,
+        borderWidth:1,
+        paddingHorizontal:20,
+        paddingVertical:10,
+        borderColor:'white'
+    },
+    signUp:{
+        color:'white',
+        fontWeight:'bold'
+    },
+    letsBeginBtn: {
+        backgroundColor:'white',
+        margin:10,
+        borderWidth:1,
+        paddingHorizontal:20,
+        paddingVertical:10,
+        borderColor:'white'
+    },
+    letsBegin:{
+        fontWeight:'bold'
+    },
+    textYellowColor: {
+        color: '#f1c11a'
+    }
+})
